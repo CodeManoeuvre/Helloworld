0da17c9b3154ddcba14a0db6d37e11becd53a6de
ofentse khampe <ofentsekhampe63@gmail.com>
2022-05-23 11:55:22 +0200

    Add project files.

---
+++ Helloworld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();//Preveents the console from closing qickly
+
+        }
+    }
+}
